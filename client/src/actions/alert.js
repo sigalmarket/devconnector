82d5a22cf17afde9f51e580b0913c94d8c76a154
sigalmarket <anthony.vella.2409.2@gmail.com>
2019-05-03 14:56:12 +0200

    Alert component & action call VALUABLE

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -11,4 +11,13 @@ export const setAlert = (msg, alertType) => dispatch => {
       id
     }
   });
+
+  setTimeout(
+    () =>
+      dispatch({
+        type: REMOVE_ALERT,
+        payload: id
+      }),
+    5000
+  );
 };
