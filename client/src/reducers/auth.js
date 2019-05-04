eab92b6719bc8010ce0834ebc8b8a21d9e3185a3
sigalmarket <anthony.vella.2409.2@gmail.com>
2019-05-04 14:12:09 +0200

    logout & navbar link

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -4,7 +4,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from '../actions/types';
 
 const initialState = {
@@ -34,15 +35,9 @@ export default function(state = initialState, action) {
         loading: false
       };
     case AUTH_ERROR:
-      localStorage.removeItem('token');
-      return {
-        ...state,
-        token: null,
-        isAuthenticated: false,
-        loading: false
-      };
     case REGISTER_FAIL:
     case LOGIN_FAIL:
+    case LOGOUT:
       localStorage.removeItem('token');
       return {
         ...state,
